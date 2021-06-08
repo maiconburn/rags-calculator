6bde6a29f1b6e8c5c5aa349feede4de0fea9f474
Maicon Ribeiro Esteves <maicon.burn@gmail.com>
2021-06-09 00:17:27 +0100

    Component Increment Decrement

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,23 @@
 import React from "react";
 import NumberOfPeople from "../components/questions/NumberOfPeople";
-//import ButtonIncrementDecrement from "../components/ButtonIncrementDecrement";
+import ButtonIncrementDecrement from "../components/ButtonIncrementDecrement";
 import NavBar from "../components/NavBar";
 import Stepper from "../components/Steeper";
 import { useForm, Controller } from "react-hook-form";
-import Button from "@material-ui/core/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-import Typography from "@material-ui/core/Typography";
-import Input from "@material-ui/core/Input";
 
 export default function Index() {
   const { control, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
-
-  const [counter, setCounter] = React.useState(1);
-
-  const handleIncrement = () => {
-    setCounter((counter) => counter + 1);
-  };
-
-  const handleDecrement = () => {
-    setCounter((counter) => counter - 1);
-  };
-
   return (
     <div>
       <NavBar />
       <NumberOfPeople />
       <form onSubmit={handleSubmit(onSubmit)}>
-        <ButtonGroup size="large" aria-label="large outlined button group">
-          <Button onClick={handleDecrement}>-</Button>
-          <Typography variant="h1">{counter}</Typography>
-          <Controller
-            name="numberOfPeople"
-            control={control}
-            render={({ field }) => (
-              <Input type="text" value={counter} {...field} />
-            )}
-          />
-          <Button onClick={handleIncrement}>+</Button>
-        </ButtonGroup>
-        <input type="submit" />
+        <Controller
+          name="numberOfPeople"
+          control={control}
+          render={({ field }) => <ButtonIncrementDecrement {...field} />}
+        />
       </form>
       <Stepper
         stepsNumber={10}
