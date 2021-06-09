b99c79e781b2990aa1fccfddc9d5d307a75aa145
Maicon Ribeiro Esteves <maicon.burn@gmail.com>
2021-06-09 13:03:21 +0100

    test

--- pages/houseInfo.js
+++ pages/houseInfo.js
@@ -83,15 +83,14 @@ export default function houseInfo() {
             onChange={handleHouseSize}
             aria-label="House size"
           >
-<<<<<<< HEAD
             <ToggleButton value="small" size="large" aria-label="Small">
-              <Tooltip title="Test">
+              <Tooltip title="1 Room" aria-label="add">
                 <span>Small</span>
               </Tooltip>
             </ToggleButton>
 
             <ToggleButton value="medium" size="large" aria-label="Medium">
-              <Tooltip title="Add" aria-label="add">
+              <Tooltip title="2-3 Rooms" aria-label="add">
                 <span>Medium</span>
               </Tooltip>
             </ToggleButton>
@@ -111,32 +110,6 @@ export default function houseInfo() {
                 <span>Halls of residence</span>
               </Tooltip>
             </ToggleButton>
-=======
-            <Tooltip title="1 Room" aria-label="add">
-              <ToggleButton value="small" size="large" aria-label="Small">
-                Small
-              </ToggleButton>
-            </Tooltip>
-            <Tooltip title="2-3 Rooms" aria-label="add">
-              <ToggleButton value="medium" size="large" aria-label="Medium">
-                Medium
-              </ToggleButton>
-            </Tooltip>
-            <Tooltip title="4+ Rooms" aria-label="add">
-              <ToggleButton value="large" size="large" aria-label="Large">
-                Large
-              </ToggleButton>
-            </Tooltip>
-            <Tooltip title="Add" aria-label="add">
-              <ToggleButton
-                value="halls"
-                size="large"
-                aria-label="Halls of residence"
-              >
-                Student Halls
-              </ToggleButton>
-            </Tooltip>
->>>>>>> 719f155548007cf461fff4057377643162fdc818
           </ToggleButtonGroup>
         </Box>
         <Grid item xs={12}>
